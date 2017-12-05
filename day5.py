7e1b9031c1a79399c1910785994ef063c83382a1
Nanor <ronan131@gmail.com>
2017-12-05 09:55:37 +0000

    Add solution for day 5

---
+++ day5.py
@@ -0,0 +1,44 @@
+#!/usr/local/bin/python3
+
+
+def part1(instructions):
+    i = 0
+    c = 0
+
+    while i >= 0 and i < len(instructions):
+        new_i = i + instructions[i]
+        instructions[i] += 1
+        i = new_i
+        c += 1
+
+    return c
+
+
+def part2(instructions):
+    i = 0
+    c = 0
+
+    while i >= 0 and i < len(instructions):
+        new_i = i + instructions[i]
+
+        if instructions[i] >= 3:
+            instructions[i] -= 1
+        else:
+            instructions[i] += 1
+
+        i = new_i
+        c += 1
+
+    return c
+
+
+def main():
+    with open('day5.txt') as f:
+        inputs = [int(line) for line in f]
+
+    print(part1(list(inputs)))
+    print(part2(list(inputs)))
+
+
+if __name__ == '__main__':
+    main()
