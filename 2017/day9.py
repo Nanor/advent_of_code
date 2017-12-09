e8fed1f92cf896724d888a408a3bdd78e932fddb
Nanor <ronan131@gmail.com>
2017-12-09 15:02:04 +0000

    Add solution for day 9 2017

---
+++ 2017/day9.py
@@ -0,0 +1,35 @@
+#!/usr/local/bin/python3
+import re
+import json
+
+
+def part1(groups):
+    groups = re.sub(r'!.', '', groups)
+    groups = re.sub(r'<[^>]*>', '', groups)
+    groups = re.sub(r'[^{}]', '', groups)
+    groups = re.sub(r'}{', '},{', groups)
+    groups = re.sub(r'{', '[', groups)
+    groups = re.sub(r'}', ']', groups)
+    groups = json.loads(groups)
+
+    def calculate(groups, count):
+        return count + sum(calculate(g, count + 1) for g in groups)
+
+    return calculate(groups, 1)
+
+
+def part2(groups):
+    groups = re.sub(r'!.', '', groups)
+    garbage = re.findall(r'<[^>]*>', groups)
+
+    return sum(len(g) - 2 for g in garbage)
+
+def main():
+    with open('day9.txt') as f:
+        groups = f.read().strip()
+
+    print(part1(groups))
+    print(part2(groups))
+
+if __name__ == '__main__':
+    main()
