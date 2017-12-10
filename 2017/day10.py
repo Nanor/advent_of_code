a9027be2aac82823d36bd0b3850a47c86423cc87
Nanor <ronan131@gmail.com>
2017-12-10 14:22:26 +0000

    Add solution for day 10

--- 2017/day10.py
+++ 2017/day10.py
@@ -1,6 +1,5 @@
 #!/usr/local/bin/python3
-import unittest
-from itertools import islice
+from functools import reduce
 
 
 class Ring:
@@ -23,39 +22,46 @@ class Ring:
             self._ring[index % self.size] = value
 
 
-class TestRing(unittest.TestCase):
+def part1(input):
+    lengths = [int(x) for x in input.split(',')]
+    ring = Ring(256)
+    current = 0
+    skip = 0
 
-    def test_get_item(self):
-        ring = Ring(5)
+    for length in lengths:
+        ring[current:current +
+             length] = reversed(ring[current:current + length])
+        current += length + skip
+        skip += 1
 
-        self.assertEqual(2, ring[2])
-        self.assertEqual(2, ring[7])
+    return ring[0] * ring[1]
 
-    def test_set_item(self):
-        ring = Ring(5)
-        ring[7] = 'foo'
 
-        self.assertEqual('foo', ring[2])
+def part2(input):
+    lengths = [ord(c) for c in input] + [17, 31, 73, 47, 23]
+    ring = Ring(256)
+    current = 0
+    skip = 0
 
-    def test_get_slice(self):
-        ring = Ring(5)
+    for length in lengths * 64:
+        ring[current:current +
+             length] = reversed(ring[current:current + length])
+        current += length + skip
+        skip += 1
 
-        self.assertEqual([1, 2, 3], ring[1:4])
-        self.assertEqual([1, 2, 3], ring[6:9])
-        self.assertEqual([0, 1, 2], ring[:3])
-        self.assertEqual([0, 1, 2, 3, 4, 0], ring[:6])
+    dense = [reduce((lambda x, y: x ^ y), ring[n:n + 16])
+             for n in range(0, 256, 16)]
+    hexes = [hex(n)[2:].zfill(2) for n in dense]
 
-    def test_set_slice(self):
-        ring = Ring(5)
+    return ''.join(hexes)
 
-        ring[1:3] = ['x', 'y']
-        self.assertEqual([0, 'x', 'y', 3, 4], ring[0:5])
 
-        ring = Ring(5)
+def main():
+    with open('day10.txt') as f:
+        input = f.read()
 
-        ring[3:7] = ['a', 'b', 'c', 'd']
-        self.assertEqual(['c', 'd', 2, 'a', 'b'], ring[0:5])
-
+    print(part1(input))
+    print(part2(input))
 
 if __name__ == '__main__':
-    unittest.main()
+    main()
