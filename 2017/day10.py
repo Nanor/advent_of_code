a2a807fae147b3df5fc7021385cfc07cea27ccc4
Nanor <ronan131@gmail.com>
2017-12-14 13:24:48 +0000

    Extract hash function from day 10

--- 2017/day10.py
+++ 2017/day10.py
@@ -22,38 +22,41 @@ class Ring:
             self._ring[index % self.size] = value
 
 
-def part1(input):
-    lengths = [int(x) for x in input.split(',')]
+def knot(input, times=1):
     ring = Ring(256)
     current = 0
     skip = 0
 
-    for length in lengths:
+    for length in input * times:
         ring[current:current +
              length] = reversed(ring[current:current + length])
         current += length + skip
         skip += 1
 
-    return ring[0] * ring[1]
+    return ring[0:256]
 
 
-def part2(input):
+def hash(input):
     lengths = [ord(c) for c in input] + [17, 31, 73, 47, 23]
-    ring = Ring(256)
-    current = 0
-    skip = 0
+    
+    result = knot(lengths, 64)
 
-    for length in lengths * 64:
-        ring[current:current +
-             length] = reversed(ring[current:current + length])
-        current += length + skip
-        skip += 1
-
-    dense = [reduce((lambda x, y: x ^ y), ring[n:n + 16])
+    dense = [reduce((lambda x, y: x ^ y), result[n:n + 16])
              for n in range(0, 256, 16)]
     hexes = [hex(n)[2:].zfill(2) for n in dense]
 
     return ''.join(hexes)
+
+
+def part1(input):
+    lengths = [int(x) for x in input.split(',')]
+    result = knot(lengths)
+
+    return result[0] * result[1]
+
+
+def part2(input):
+    return hash(input)
 
 
 def main():
@@ -62,6 +65,7 @@ def main():
 
     print(part1(input))
     print(part2(input))
+
 
 if __name__ == '__main__':
     main()
