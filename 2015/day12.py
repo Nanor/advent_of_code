20f24d3c7297dda9cd6f79b9a657079193476c52
Nanor <ronan131@gmail.com>
2017-12-16 18:27:04 +0000

    Add solution for 2015 day 12

---
+++ 2015/day12.py
@@ -0,0 +1,30 @@
+#!/usr/local/bin/python3
+import json
+
+
+def sum_json(json, ignore_red=False):
+    if isinstance(json, int):
+        return json
+    elif isinstance(json, list):
+        return sum(sum_json(j, ignore_red=ignore_red) for j in json)
+    elif isinstance(json, dict):
+        if 'red' in json.values() and ignore_red:
+            return 0
+        else:
+            return sum(sum_json(j, ignore_red=ignore_red) for j in json.values())
+    elif isinstance(json, str):
+        return 0
+
+    raise NotImplementedError(type(json))
+
+
+def main():
+    with open('day12.txt') as file:
+        input = json.load(file)
+
+    print(sum_json(input))
+    print(sum_json(input, ignore_red=True))
+
+
+if __name__ == '__main__':
+    main()
