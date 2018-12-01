961d9b96dce6675ee12d782346ce0c42cb9e3e3a
Ronan Forman <ronan131@gmail.com>
2018-12-01 13:20:04 +0000

    Add solution for 2018 day 1

---
+++ 2018/day1.rs
@@ -0,0 +1,42 @@
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
+
+fn part1() -> i32 {
+  let f = File::open("day1.txt").unwrap();
+  let file = BufReader::new(&f);
+
+  let mut counter: i32 = 0;
+  for (_, line) in file.lines().enumerate() {
+    let num: i32 = line.unwrap().parse().expect("NaN");
+    counter = counter + num;
+  }
+
+  return counter;
+}
+
+fn part2() -> i32 {
+  let mut counter: i32 = 0;
+  let mut values: HashMap<i32, bool> = HashMap::new();
+
+  loop {
+    let f = File::open("day1.txt").unwrap();
+    let file = BufReader::new(&f);
+
+    for (_, line) in file.lines().enumerate() {
+      let num: i32 = line.unwrap().parse().expect("NaN");
+      counter = counter + num;
+
+      match values.get(&counter) {
+        Some(_) => return counter,
+        None => values.insert(counter, true),
+      };
+    }
+  }
+}
+
+fn main() {
+  println!("{}", part1());
+  println!("{}", part2());
+}
