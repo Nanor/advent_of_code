0b284755e9deeacd82b4df219d11e652bd667de9
Ronan Forman <ronan131@gmail.com>
2019-12-03 11:13:18 +0000

    Add solution for 2019 day 3

---
+++ 2019/src/day3.ts
@@ -0,0 +1,90 @@
+import { Input } from "./types";
+
+type Direction = "R" | "D" | "L" | "U";
+
+type Wire = {
+  dir: Direction;
+  len: number;
+}[];
+
+type Point = {
+  x: number;
+  y: number;
+  dist: number;
+};
+
+const asWires = (input: Input): Wire[] =>
+  input
+    .asLines()
+    .map(l => l.split(","))
+    .map(wire =>
+      wire.map(x => ({ dir: x[0] as Direction, len: parseInt(x.slice(1), 10) }))
+    );
+
+const traceWire = (wire: Wire) => {
+  let x = 0;
+  let y = 0;
+  let dist = 0;
+  const points: Point[] = [];
+
+  wire.forEach(({ dir, len }) => {
+    const [dx, dy] = {
+      U: [0, -1],
+      D: [0, 1],
+      L: [-1, 0],
+      R: [1, 0]
+    }[dir];
+
+    for (let n = 0; n < len; n++) {
+      x += dx;
+      y += dy;
+      dist += 1;
+      points.push({ x, y, dist });
+    }
+  });
+
+  return points;
+};
+
+const getHits = (wires: Wire[]): Point[] => {
+  const points = wires.map(w => traceWire(w));
+
+  const points1 = points[0];
+  const points2 = points[1];
+
+  const hits: Point[] = [];
+
+  points1.forEach(p1 => {
+    points2.forEach(p2 => {
+      if (p1.x === p2.x && p1.y === p2.y) {
+        hits.push({ x: p1.x, y: p2.y, dist: p1.dist + p2.dist });
+      }
+    });
+  });
+
+  return hits;
+};
+
+let hits: Point[];
+
+export const part1 = (input: Input) => {
+  const wires = asWires(input);
+  hits = getHits(wires);
+
+  const closest = hits
+    .map(({ x, y }) => Math.abs(x) + Math.abs(y))
+    .reduce((acc, x) => (acc < x ? acc : x), Number.MAX_VALUE);
+
+  return closest;
+};
+
+export const part2 = (input: Input) => {
+  // const wires = asWires(input);
+  // const hits = getHits(wires);
+
+  const closest = hits
+    .map(({ dist }) => dist)
+    .reduce((acc, x) => (acc < x ? acc : x), Number.MAX_VALUE);
+
+  return closest;
+};
