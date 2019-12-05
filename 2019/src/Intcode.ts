788a1968795bbeac168494860695b08b515e8287
Ronan Forman <ronan131@gmail.com>
2019-12-05 11:32:55 +0000

    Refactor intcode

--- 2019/src/Intcode.ts
+++ 2019/src/Intcode.ts
@@ -17,17 +17,23 @@ const intcode = (program: number[], input: number[] = []) => {
     data[pos] = value;
   };
 
-  const calc = (op: number, vals: number[]) => {
-    let res: number;
+  const digitAt = (number: number, pos: number) => {
+    return Math.floor(number / Math.pow(10, pos)) % 10;
+  };
+
+  const param = (p: number) => {
+    const op = read(0, false);
+    return read(p, digitAt(op, p + 1) === 0);
+  };
+
+  const calc = (op: number) => {
     switch (op) {
       case 1:
-        res = vals[0] + vals[1];
-        write(3, res);
+        write(3, param(1) + param(2));
         index += 4;
         return;
       case 2:
-        res = vals[0] * vals[1];
-        write(3, res);
+        write(3, param(1) * param(2));
         index += 4;
         return;
       case 3:
@@ -35,38 +41,34 @@ const intcode = (program: number[], input: number[] = []) => {
         index += 2;
         return;
       case 4:
-        console.log(vals[0]);
+        console.log(param(1));
         index += 2;
         return;
       case 5:
-        if (vals[0]) {
-          index = vals[1];
+        if (param(1)) {
+          index = param(2);
         } else {
-          index += 3
+          index += 3;
         }
         return;
       case 6:
-        if (vals[0] === 0) {
-          index = vals[1];
+        if (param(1) === 0) {
+          index = param(2);
         } else {
-          index += 3
+          index += 3;
         }
         return;
       case 7:
-        write(3, vals[0] < vals[1] ? 1 : 0);
+        write(3, param(1) < param(2) ? 1 : 0);
         index += 4;
         return;
       case 8:
-        write(3, vals[0] === vals[1] ? 1 : 0);
+        write(3, param(1) === param(2) ? 1 : 0);
         index += 4;
         return;
       default:
         throw new Error(`Unknown opcode ${op}`);
     }
-  };
-
-  const digitAt = (number: number, pos: number) => {
-    return Math.floor(number / Math.pow(10, pos)) % 10;
   };
 
   while (true) {
@@ -75,10 +77,7 @@ const intcode = (program: number[], input: number[] = []) => {
       return data;
     }
 
-    const val1 = read(1, digitAt(op, 2) === 0);
-    const val2 = read(2, digitAt(op, 3) === 0);
-
-    calc(op % 100, [val1, val2]);
+    calc(op % 100);
   }
 };
 
