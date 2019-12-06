c07b04337ab32b2e06ea27f966387cc0a06fca63
Ronan Forman <ronan131@gmail.com>
2019-12-06 11:02:22 +0000

    Add solution for 2019 day 6

---
+++ 2019/src/day6.ts
@@ -0,0 +1,51 @@
+import { Input } from "./types";
+
+export const part1 = (input: Input) => {
+  const data = input.asLines().map(l => l.split(")"));
+
+  const checked = [];
+  const unchecked = ["COM"];
+  const orbitCount = { COM: 0 };
+
+  while (unchecked.length > 0) {
+    const obj = unchecked.pop();
+
+    const subObjs = data
+      .filter(([obj1, obj2]) => obj1 === obj)
+      .map(([obj1, obj2]) => obj2);
+
+    subObjs.forEach(obj2 => {
+      orbitCount[obj2] = orbitCount[obj] + 1;
+    });
+
+    checked.push(obj);
+    unchecked.push(...subObjs);
+  }
+
+  return Object.values(orbitCount).reduce((acc, x) => acc + x);
+};
+
+const getPath = (data, start) => {
+  const path = [];
+
+  let curr = start;
+  while (true) {
+    curr = data.find(([obj1, obj2]) => obj2 === curr)[0];
+    path.push(curr);
+
+    if (curr === "COM") {
+      return path;
+    }
+  }
+};
+
+export const part2 = (input: Input) => {
+  const data = input.asLines().map(l => l.split(")"));
+
+  const youPath = getPath(data, "YOU");
+  const sanPath = getPath(data, "SAN");
+
+  const common = youPath.find(obj => sanPath.includes(obj));
+
+  return youPath.indexOf(common) + sanPath.indexOf(common);
+};
