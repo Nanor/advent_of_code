21388a06ae39cb7ef0a3a86ed5920936d130b465
Ronan Forman <ronan.forman@carbon.ci>
2021-12-01 13:37:28 +0000

    Setup 2021

---
+++ 2021/src/days/day1.ts
@@ -0,0 +1,5 @@
+import { Input } from "../input";
+
+export const part1 = (input: Input) => {};
+
+export const part2 = (input: Input) => {};
---
+++ 2021/src/index.ts
@@ -0,0 +1,18 @@
+import getInput from "./input";
+import * as days from "./days";
+
+const day = process.argv[2];
+const code = days[`day${day}`];
+
+const main = async () => {
+  const input = await getInput(day);
+
+  if ("part1" in code) {
+    console.log(code.part1(input));
+  }
+  if ("part2" in code) {
+    console.log(code.part2(input));
+  }
+};
+
+main();
