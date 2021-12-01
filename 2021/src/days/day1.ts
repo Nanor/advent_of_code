5d90c120a3fadbdcc8e1e01e7a5228c0ac49d22c
Ronan <ronan131@gmail.com>
2021-12-01 13:47:00 +0000

    Add 2021 day 1

--- 2021/src/days/day1.ts
+++ 2021/src/days/day1.ts
@@ -1,5 +1,29 @@
 import { Input } from "../input";
 
-export const part1 = (input: Input) => {};
+export const part1 = (input: Input) => {
+  let increases = 0;
+  let lastDepth = null;
+  input.asNumbers().forEach((depth) => {
+    if (lastDepth && depth > lastDepth) increases += 1;
 
-export const part2 = (input: Input) => {};
+    lastDepth = depth;
+  });
+
+  return increases;
+};
+
+export const part2 = (input: Input) => {
+  let increases = 0;
+  let lastDepth = null;
+
+  const depths = input.asNumbers();
+  depths.forEach((depth, i) => {
+    const depthSum = depths.slice(i, i + 3).reduce((x, y) => x + y);
+
+    if (lastDepth && depthSum > lastDepth) increases += 1;
+
+    lastDepth = depthSum;
+  });
+
+  return increases;
+};
