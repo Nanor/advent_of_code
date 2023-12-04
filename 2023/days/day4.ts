8f7fbaec37e6882e7aabb95aa2ef8c49d1dc9770
Ronan <ronan131@gmail.com>
2023-12-04 12:28:45 +0000

    Add solution for 2023 day 4

---
+++ 2023/days/day4.ts
@@ -0,0 +1,32 @@
+import { Input } from "../input";
+
+const parse = (input: Input) => {
+  return input.asLines().map((line) => {
+    const parts = line.split(/[:|]/);
+
+    return {
+      winning: parts[1].split(" ").filter(Boolean),
+      actual: parts[2].split(" ").filter(Boolean),
+    };
+  });
+};
+
+export const part1 = (input: Input) =>
+  parse(input)
+    .map((card) => card.actual.filter((n) => card.winning.includes(n)).length)
+    .map((count) => (count ? Math.pow(2, count - 1) : 0))
+    .reduceRight((x, y) => x + y);
+
+export const part2 = (input: Input) => {
+  const cards = parse(input).map((c) => ({ ...c, count: 1 }));
+
+  cards.forEach((card, i) => {
+    const winnings = card.actual.filter((n) => card.winning.includes(n)).length;
+
+    cards
+      .slice(i + 1, i + 1 + winnings)
+      .forEach((c) => (c.count += card.count));
+  });
+
+  return cards.reduce((acc, card) => acc + card.count, 0);
+};
