7f5c618e8828add2a6443e03c12e19c117f0dd17
Ronan <ronan131@gmail.com>
2023-12-18 16:11:10 +0000

    Add solution for 2023 day 18

---
+++ 2023/src/days/day18.ts
@@ -0,0 +1,64 @@
+import type { Input } from "../input";
+import { Coord, Dir, sum } from "../utils";
+
+const solve = (instructions: { dir: Dir; dist: number }[]) => {
+  const coords = instructions.reduce<Coord[]>(
+    (acc, { dir, dist }) => {
+      let last = { ...acc.at(-1)! };
+
+      switch (dir) {
+        case "L":
+          last.x -= dist;
+          break;
+        case "R":
+          last.x += dist;
+          break;
+        case "U":
+          last.y -= dist;
+          break;
+        case "D":
+          last.y += dist;
+          break;
+      }
+
+      return [...acc, last];
+    },
+    [{ x: 0, y: 0 }]
+  );
+
+  return (
+    coords
+      .slice(0, -1)
+      .map((_, i) => {
+        const a = coords[i];
+        const b = coords[i + 1];
+
+        return (a.x * b.y - b.x * a.y) / 2;
+      })
+      .reduce(sum) +
+    instructions.map((i) => i.dist).reduce(sum) / 2 +
+    1
+  );
+};
+
+export const part1 = (input: Input) => {
+  const instructions = input.asLines().map((l) => {
+    const [_, dir, dist] = l.match(/^([UDLR]) (\d+) \(#(\S{6})\)$/)!;
+    return { dir: dir as Dir, dist: parseInt(dist) };
+  });
+
+  return solve(instructions);
+};
+
+export const part2 = (input: Input) => {
+  const instructions = input.asLines().map((l) => {
+    const color = l.match(/^([UDLR]) (\d+) \(#(\S{6})\)$/)![3];
+
+    return {
+      dir: "RDLU"[parseInt(color[5])] as Dir,
+      dist: parseInt(color.slice(0, 5), 16),
+    };
+  });
+
+  return solve(instructions);
+};
